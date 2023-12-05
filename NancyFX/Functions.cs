2eed07f7a6c9ac5b02482f1c6bd644e43b14158d
acksmoke <ackura@bu.edu>
2023-12-04 20:51:33 -0500

    Clutch fix

--- NancyFX/Functions.cs
+++ NancyFX/Functions.cs
@@ -1,15 +1,16 @@
-using FireSharp.Interfaces;
-using FireSharp.Response;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace NancyFX
 {
     public static class Functions
     {
-        public static async Task AddUser(IFirebaseClient client, int id, string firstName, string lastName, string description, 
-                                  string email, string profilePicture, string major, string minor, string[] coursesTaken)
+        public static async Task<Users> AddUser(IFirebaseClient client, int id, string firstName, string lastName, string description, 
+                                                string email, string profilePicture, string major, string minor, string[] coursesTaken)
         {
             var user = new Users
             {
@@ -25,11 +26,12 @@ namespace NancyFX
             };
 
             SetResponse response = await client.SetAsync("Users/" + user.id, user);
-            Users result = response.ResultAs<Users>(); // Assuming you need the result for further processing
+            return response.ResultAs<Users>(); // Return the user object
         }
 
-        public static async Task AddService(IFirebaseClient client, int serviceId, string serviceName, string shortServiceDescription, decimal price, int userId, 
-                                     string location, string serviceType, double review, bool deleted) {
+        public static async Task<Services> AddService(IFirebaseClient client, int serviceId, string serviceName, string shortServiceDescription, decimal price, int userId, 
+                                                      string location, string serviceType, double review, bool deleted)
+        {
             var service = new Services
             {
                 serviceId = serviceId,
@@ -44,59 +46,38 @@ namespace NancyFX
             };
 
             SetResponse response = await client.SetAsync("Services/" + service.serviceId, service);
-            Services result = response.ResultAs<Services>(); // Assuming you need the result for further processing
+            return response.ResultAs<Services>(); // Return the service object
         }
+
         public static async Task<object> GetServices(IFirebaseClient client)
         {
             try
             {
                 string path = "Services/";
-
                 FirebaseResponse response = await client.GetAsync(path);
                 var serviceData = response.ResultAs<Dictionary<string, Services>>();
-
-                if (serviceData != null)
-                {
-                    // Convert the Dictionary to a List or another suitable format for JSON serialization
-                    return serviceData.Values.ToList();
-                }
-                else
-                {
-                    return new { message = "No service data found." };
-                }
+                return serviceData != null ? serviceData.Values.ToList() : new { message = "No service data found." };
             }
             catch (Exception ex)
             {
-                // Log the exception and return an error message
                 Console.WriteLine($"Error retrieving service data: {ex.Message}");
                 return new { error = "An error occurred while retrieving data." };
             }
         }
 
-
-        public static async Task GetUsers(IFirebaseClient client, int userId)
+        public static async Task<Users> GetUsers(IFirebaseClient client, int userId)
         {
             try
             {
                 string path = $"Users/{userId}";
-
                 FirebaseResponse response = await client.GetAsync(path);
-                Users userData = response.ResultAs<Users>();
-
-                if (userData != null)
-                {
-                    Console.WriteLine($"User Data retrieved: {userData.firstName}");
-                }
-                else
-                {
-                    Console.WriteLine("No user data found at the specified path.");
-                }
+                return response.ResultAs<Users>(); // Return the user object
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error retrieving user data: {ex.Message}");
+                return null; // Or handle the error as you see fit
             }
         }
     }
 }
-
